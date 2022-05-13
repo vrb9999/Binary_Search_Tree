6a35a2595befb0e75a89bbc5de11ed74dcb314c1
vrb9999 <vbiradar9999@gmail.com>
2022-05-14 01:24:28 +0530

    UC-2 Day-15 Check all elements are added in BST using size

---
+++ BST_UsingSize/BST_UsingSize/BinarySearchTree.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BST_UsingSize
+{
+    class BinarySearchTree<T> where T : IComparable
+    {
+        int treeSize = 0;
+
+        public INode<T> root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void Insert(int element)
+        {
+            INode<T> newNode = new INode<T>();
+            newNode.NodeData = element;
+            if (root == null)
+            {
+                root = newNode;
+                treeSize++;
+            }
+            else
+            {
+                INode<T> newRoot = root;
+                INode<T> parent;
+                while (true)
+                {
+                    parent = newRoot;
+                    if (element < newRoot.NodeData)
+                    {
+                        newRoot = newRoot.LeftTree;
+                        if (newRoot == null)
+                        {
+                            parent.LeftTree = newNode;
+                            treeSize++;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        newRoot = newRoot.RightTree;
+                        if (newRoot == null)
+                        {
+                            parent.RightTree = newNode;
+                            treeSize++;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        public void SizeOfTree()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Binary Search Tree is Empty");
+            }
+            Console.WriteLine("\nSize of tree is : " + treeSize);
+        }
+        public void Traverse(INode<T> Root)
+        {
+            if (Root != null)
+            {
+                Traverse(Root.LeftTree);
+                Console.Write(Root.NodeData + " ");
+                Traverse(Root.RightTree);
+            }
+        }
+    }
+}
---
+++ BST_UsingSize/BST_UsingSize/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BST_UsingSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>();
+            Console.WriteLine("Root : 56 ");
+            binarySearch.Insert(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
+            Console.Write("\n:: BST Elements :: \n");
+            binarySearch.Traverse(binarySearch.root);
+            binarySearch.SizeOfTree();
+        }
+    }
+}
